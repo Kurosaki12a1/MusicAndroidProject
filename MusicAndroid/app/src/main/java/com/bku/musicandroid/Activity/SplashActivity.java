8d5c926a42219e583ca3668882d3313e2a8cb382
Phan Tran Thai Son <phantranthaison12@gmail.com>
2018-05-20 14:59:30 +0700

    Add splash screen and icon for app

---
+++ MusicAndroid/app/src/main/java/com/bku/musicandroid/Activity/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.bku.musicandroid.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
